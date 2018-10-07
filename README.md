35bd89c02e5d3346ba94cea4c2644ea5bc5289d0
Jose-Luis-Me-Le <jolumele56@gmail.com>
2018-10-07 21:06:06 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+
+##Aplicacion del clima con comandos node
+
+Ejecutar con el comando ```npm install``` para las librerias
+
+###Ejemplo:
+```
+node app -d "Madrid España"
---
+++ app.js
@@ -0,0 +1,35 @@
+const lugar = require('./lugar/lugar');
+const clima = require('./clima/clima');
+
+const argv = require('yargs').options({
+    direccion: {
+        alias: 'd',
+        des: 'Direccion de la ciudad que obtenemos el clima',
+        demand: true
+    }
+
+}).argv;
+
+let getInfo = async(direccion) => {
+
+    try {
+        let coors = await lugar.getLugarLatLng(direccion);
+        let temp = await clima.getClima(coors.lat, coors.lng);
+
+        return `El clima en ${coors.direccion} es de ${temp}`
+    } catch (e) {
+        return `No se pudo determinar el clima en ${direccion}`;
+    }
+}
+
+getInfo(argv.direccion)
+    .then(mensaje => console.log(mensaje))
+    .catch(e => console.log(e));
+
+
+
+lugar.getLugarLatLng(argv.direccion)
+    .then(resp => {
+        console.log(resp);
+    })
+    .catch(e => console.log(e));
